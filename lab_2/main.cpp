e68ac74d46ea5a5e6f30fc0313aff8d1da259d3c
Максим Трушников <dein.tenemurl@gmail.com>
2024-10-02 22:53:45 +0200

    add: laba 2 :_)

---
+++ lab_2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"enter a number 'a': ";
+    int a;
+    std::cin>>a;
+    std::cout<<"enter a number 'h': ";
+    int h;
+    std::cin>>h;
+    std::cout << "S=" << a*h*0.5 <<std::endl;
+    return 0;
+    
+     
+}
