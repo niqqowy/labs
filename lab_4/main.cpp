c568ee10018c40b13b34ae9bbe99507ccad5eacd
Максим Трушников <dein.tenemurl@gmail.com>
2024-10-12 10:21:15 +0200

    add: task 1 2 3

---
+++ lab_4/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+/*  //task 1
+int main() {
+    int a, b, c;
+    cout<<"enter a: ";
+    cin >> a;
+    cout<<"enter b: ";
+    cin >> b;
+    cout<<"enter c: ";
+    cin >> c;
+    if (a > 0 && b > 0 && c > 0){
+        if( (a%c==0) && (b%c==0)){
+            cout<< ((a+b)/c);
+        }
+        else if((a%c==0) && (b % c!=0)){
+            cout<< (a/c+b);
+        }
+        else {
+        cout<< (a-b-c);
+        }
+    }
+    else {
+        cout<<"you need to enter a natural number";
+    }
+   
+    return 0;
+}
+*/
+
+ /* //task 2
+int main(){
+    cout<<"enter a number ";
+    int N;
+    cin>>N;
+    switch(N){
+        case 0: cout<<"zero"; break;
+        case 1: cout<<"one"; break;
+        case 2: cout<<"two"; break;
+        case 3: cout<<"three"; break;
+        case 4 : cout<<"four"; break;
+        case 5 : cout<<"five"; break;
+        case 6 : cout<<"six"; break;
+        case 7 : cout<<"seven"; break;
+        case 8 : cout<<"eight"; break;
+        case 9 : cout<<"nine"; break;
+        default: cout<<"error";
+    }
+    return 0;
+
+}
+*/
+
+
+/*    //task 3
+int main(){
+    double x;
+    cout<<"enter -1 or 1: ";
+    cin>>x;
+    if (x==-1){
+        cout<<"Negative number";
+    }
+    else if (x==1){
+        cout<<"Positive number";
+    }
+    else{
+        cout<<"incorrect number";
+    }
+    return 0;
+}
+*/
+
+
